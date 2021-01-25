1483e0e38d52692c59828e33ebd67c41770aef1b
Michael Clapham <michaeljclapham@gmail.com>
2021-01-25 16:29:27 +0000

    Add scully

---
+++ scully.ng-pages-test.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "ng-pages-test",
+  outDir: './dist/static',
+  routes: {
+  }
+};
